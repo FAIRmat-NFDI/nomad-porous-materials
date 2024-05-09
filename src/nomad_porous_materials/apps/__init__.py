64cd12ac2f5e7cf88fbc32ffaaafdc8885f9a46d
Lauri Himanen <lauri.himanen@gmail.com>
2024-05-09 14:17:44 +0300

    Added MOF app.

--- src/nomad_porous_materials/apps/__init__.py
+++ src/nomad_porous_materials/apps/__init__.py
@@ -1,24 +1,159 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Columns,
+    Dashboard,
+    FilterMenu,
+    FilterMenus,
+    Filters,
+    SearchSyntaxes,
+    WidgetHistogram,
+    WidgetPeriodicTable,
+    WidgetTerms,
+    Layout
+)
 
-
-myapp = AppEntryPoint(
-    name='MyApp',
+mofapp = AppEntryPoint(
+    name='MOF',
     description='App defined using the new plugin mechanism.',
     app=App(
-        label='MyApp',
-        path='myapp',
-        category='simulation',
+        label='Metal-Organic Frameworks',
+        path='mofs',
+        description='Search metal-organic frameworks (MOFs)',
+        readme='''
+            This page allows you to search **metal-organic framework
+            (MOF) data** within NOMAD. The filter menu on the left
+            and the shown default columns are specifically designed
+            for MOF exploration. The dashboard directly shows useful
+            interactive statistics about the data.''',
+        category='Use Cases',
+        filters=Filters(
+            exclude=['mainfile', 'entry_name', 'combine']
+        ),
+        filters_locked={'results.material.topology.label': 'MOF'},
+        search_syntaxes=SearchSyntaxes(
+            exclude=['free_text']
+        ),
         columns=Columns(
-            selected=['entry_id'],
+            selected=['results.material.chemical_formula_iupac', 'mainfile', 'authors'],
             options={
-                'entry_id': Column(),
+                'results.material.chemical_formula_iupac': Column(label='Formula'),
+                'mainfile': Column(),
+                'upload_create_time': Column(label='Upload time'),
+                'authors': Column(),
+                'comment': Column(),
+                'datasets': Column(),
+                'published': Column(label='Access'),
             },
         ),
         filter_menus=FilterMenus(
             options={
                 'material': FilterMenu(label='Material'),
+                'elements': FilterMenu(
+                    label='Elements / Formula',
+                    level=1,
+                    size='xl',
+                ),
+                'structure': FilterMenu(
+                    label='Structure',
+                    level=1,
+                ),
+                'electronic': FilterMenu(
+                    label='Electronic Properties',
+                ),
+                'author': FilterMenu(
+                    label='Author / Origin / Dataset',
+                    size='m',
+                ),
+                'metadata': FilterMenu(
+                    label='Visibility / IDs / Schema',
+                ),
+                'optimade': FilterMenu(
+                    label='Optimade',
+                    size='m',
+                ),
             }
         ),
-    ),
+        dashboard=Dashboard(
+            widgets=[
+                WidgetPeriodicTable(
+                    layout={
+                        'lg': Layout(h=9, w=15, x=0, y=0),
+                        'md': Layout(h=8, w=11, x=0, y=0),
+                        'sm': Layout(h=6, w=9, x=0, y=0),
+                        'xl': Layout(h=9, w=19, x=0, y=0),
+                        'xxl': Layout(h=10, w=25, x=0, y=0),
+                    },
+                    quantity='results.material.elements',
+                    scale='linear'
+                ),
+                WidgetTerms(
+                    layout={
+                        'lg': Layout(h=9, w=9, x=15, y=0),
+                        'md': Layout(h=8, w=7, x=11, y=0),
+                        'sm': Layout(h=6, w=3, x=9, y=0),
+                        'xl': Layout(h=9, w=11, x=19, y=0),
+                        'xxl': Layout(h=10, w=11, x=25, y=0),
+                    },
+                    quantity='results.material.topology.sbu_type',
+                    scale='linear',
+                    title='SBU type',
+                    showinput=True,
+                ),
+                WidgetHistogram(
+                    layout={
+                        'lg': Layout(h=5, w=12, x=0, y=9),
+                        'md': Layout(h=4, w=9, x=0, y=8),
+                        'sm': Layout(h=3, w=6, x=0, y=6),
+                        'xl': Layout(h=5, w=15, x=0, y=9),
+                        'xxl': Layout(h=6, w=19, x=0, y=10),
+                    },
+                    quantity='results.material.topology.pore_limiting_diameter',
+                    scale='linear',
+                    nbins=30,
+                    showinput=True,
+                ),
+                WidgetHistogram(
+                    layout={
+                        'lg': Layout(h=5, w=12, x=0, y=14),
+                        'md': Layout(h=4, w=9, x=9, y=8),
+                        'sm': Layout(h=3, w=6, x=6, y=6),
+                        'xl': Layout(h=5, w=15, x=0, y=14),
+                        'xxl': Layout(h=6, w=17, x=19, y=10),
+                    },
+                    quantity='results.material.topology.largest_cavity_diameter',
+                    scale='linear',
+                    nbins=30,
+                    showinput=True,
+                ),
+                WidgetHistogram(
+                    layout={
+                        'lg': Layout(h=5, w=12, x=11, y=9),
+                        'md': Layout(h=4, w=9, x=0, y=12),
+                        'sm': Layout(h=3, w=6, x=0, y=9),
+                        'xl': Layout(h=5, w=15, x=15, y=9),
+                        'xxl': Layout(h=6, w=19, x=0, y=16),
+                    },
+                    quantity='results.material.topology.accessible_surface_area',
+                    scale='linear',
+                    nbins=30,
+                    showinput=True,
+                ),
+                WidgetHistogram(
+                    layout={
+                        'lg': Layout(h=5, w=12, x=11, y=14),
+                        'md': Layout(h=4, w=9, x=9, y=12),
+                        'sm': Layout(h=3, w=6, x=6, y=9),
+                        'xl': Layout(h=5, w=15, x=15, y=14),
+                        'xxl': Layout(h=6, w=17, x=19, y=16),
+                    },
+                    quantity='results.material.topology.void_fraction',
+                    scale='linear',
+                    nbins=30,
+                    showinput=True,
+                ),
+            ]
+        )
+    )
 )
--- src/nomad_porous_materials/normalizers/__init__.py
+++ src/nomad_porous_materials/normalizers/__init__.py
@@ -1,17 +1,38 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from nomad.config.models.plugins import NormalizerEntryPoint
 from pydantic import Field
 
 
-class MyNormalizerEntryPoint(NormalizerEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+class PorosityNormalizerEntryPoint(NormalizerEntryPoint):
+    min_n_atoms: int = Field(20, description='Minimum number of atoms allowed for running the normalizer.')
+    max_n_atoms: int = Field(5000, description='Maximum number of atoms allowed for running the normalizer.')
+    min_pld: int = Field(1.86, description='Minimum pore limiting diameter for running the normalizer.')
 
     def load(self):
-        from nomad_porous_materials.normalizers.mynormalizer import MyNormalizer
+        from nomad_porous_materials.normalizers.porositynormalizer import (
+            PorosityNormalizer,
+        )
 
-        return MyNormalizer(**self.dict())
+        return PorosityNormalizer()
 
 
-mynormalizer = MyNormalizerEntryPoint(
-    name='MyNormalizer',
-    description='Normalizer defined using the new plugin mechanism.',
+porositynormalizer = PorosityNormalizerEntryPoint(
+    name='PorosityNormalizer',
+    description='Normalizer that automatically extracts properties from porous materials.',
 )
